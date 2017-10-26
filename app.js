7e968c3edea78fb059a0f55a7e9d7ab554668dd5
jasperwoo <jasperwoo516@gmail.com>
2017-10-26 09:37:20 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 3000;
+
+//ROUTES FOR API
+
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+router.get('/date', function(req, res) {
+	var date = new Date();
+	var year = date.getFullYear();
+	var month = date.getMonth();
+	var day = date.getDate();
+	var hour = date.getHours();
+	var str = "Time now is " + year + month + day + hour;
+	res.json({ 
+		year: year,
+		month: month,
+		day: day,
+		hour: hour,
+		});
+});
+
+app.use('/api', router);
+
+app.listen(3000, function(){
+	console.log('running');
+});
